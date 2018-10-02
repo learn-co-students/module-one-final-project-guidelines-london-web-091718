c9d4496444e897c19fc6998e119a5979ec15e6ac
wimbleclaw <aisha305@hotmail.co.uk>
2018-10-02 17:42:14 +0100

    removing accessing lib error

--- config/environment.rb
+++ config/environment.rb
@@ -2,5 +2,5 @@ require 'bundler'
 Bundler.require
 
 ActiveRecord::Base.establish_connection(adapter: 'sqlite3', database: 'db/test.db')
-require_all 'lib'
-require_all 'app'
+require_all '../lib'
+require_all '../app'
