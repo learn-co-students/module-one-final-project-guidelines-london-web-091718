4e628aaffc5823f5b7f1179b732d857c17a21c82
Ed Putans <edputans@gmail.com>
2018-10-02 20:14:33 +0100

    changes done

--- app/CLI.rb
+++ app/CLI.rb
@@ -5,8 +5,8 @@ require 'json'
 require "pry"
 
 
-def exit?(hash,parameter)
-  if hash[parameter]=="exit"
+def exit?(parameter)
+  if parameter=="exit"
     puts "Goodbye! Thank you for visiting our tech jobsearch."
     exit(0)
   end
@@ -42,6 +42,12 @@ def saving_query(user,language)
   user
 end
 
+def make_query_empty?(query)
+  #CURRENTLY UNUSED
+  if query == 0
+    query = nil
+  end
+end
 
 def welcome_user
   #returns user
@@ -53,47 +59,37 @@ def welcome_user
   user_search
 end
 
-def make_query_empty?(query)
-  #CURRENTLY UNUSED
-  if query == 0
-    query = nil
-  end
-end
+
 
 def search_query(user)
-  #as of now this method does not save anything to db, just searches.
-  # puts "Would you like to search by [C]ity, [L]anguage or [J]ob keyword? /n [E]xit"
   city = ''
   language = ''
   job = ''
   search_parameter=Hash.new(0)
-  #TRY USING WHILE OR SOMETHING THAT DOES NOT EXIT
-  #Also try if..
+
 
   puts Rainbow("Please enter the city:").green
   puts "Press enter to search by language instead. Enter 'exit' to exit"
   city = STDIN.gets.chomp
-  search_parameter  [:city]=city
+  search_parameter[:city]=city
 
-  exit?(search_parameter,:city)
+  exit?(city)
   puts Rainbow("Please enter your favourite programming language:").green
   puts "Press enter to search by keyword instead. Enter 'exit' to exit"
   lang = STDIN.gets.chomp
   search_parameter[:language]=lang
 
-  exit?(search_parameter,:language)
+  exit?(lang)
   puts Rainbow("Please enter a keyword e.g 'Full-Stack'").green
   puts "Press enter if you have finished. Enter 'exit' to exit"
   keywords = STDIN.gets.chomp
   search_parameter[:keywords]=keywords
 
-  exit?(search_parameter,:keywords)
-
-  #when writing calling method check for spelling (i.e. join(-), split, join(' '), etc...)
-  # "Goodbye! Thank you for visiting our tech jobsearch."
-
+  exit?(keywords)
+  puts search_parameter
   would_you_like_to_save(user, search_parameter)
   return search_parameter
+
 end
 
 
@@ -110,7 +106,77 @@ end
       end
   end
 
+
+  def job_search_results(job_query)
+    puts "Loading Jobs..."
+    url = Job.base_url("#{job_query[:keywords]}#{job_query[:language]},#{job_query[:city]}")
+    response = RestClient.get(url)
+    puts url
+    JSON.parse(response)
+    #binding.pry
+  #  binding.pry
+  end
+
+  def result_list(job_search_results)
+    list_of_results= []
+    i = 1
+    job_search_results.each do |result|
+      Job.format_result(result,i, false)
+      i+=1
+      list_of_results << result
+    end
+  end
+
+  def chosen_job(list_of_results)
+    chosen_job_num = 800
+    while chosen_job_num != 'q' && (chosen_job_num.is_a? Integer)
+      puts "Select a number from the above list to view further job details"
+      #check if the user selected the correct listing
+      chosen_job_num = gets.chomp
+      chosen_job = list_of_results[chosen_job_num.to_i - 1]
+      puts Job.format_result(list_of_results[chosen_job_num.to_i - 1], chosen_job_num, true)
+      puts ""
+      #offer the user to open the listing in his browser using the :url
+      return chosen_job
+    end
+  end
+
+
+  # =========================================================
+
+  def more_results_with_error_test(chosen_job)
+    puts "Would you like to see more info about the city?"
+    city_more=gets.chomp
+    if city_more=="y"
+      puts 'Loading your city information...'
+      city_variable = chosen_job['location'].downcase.split(",").split("(").split("-")[0][0][0].split(" ").join("-")
+      # binding.pry
+        begin
+          resp = RestClient.get("https://api.teleport.org/api/urban_areas/slug:#{city_variable}/scores/")
+        rescue RestClient::Unauthorized, RestClient::Forbidden => err
+          puts 'Access denied'
+          return err.response
+        rescue RestClient::ExceptionWithResponse => err
+          puts 'Sorry, we are unable to find any info on that City at the moment :('
+          #if it fails to find anything, take the user back to the search screen (run method on line 20 again)
+          # show sad_cat.jpg
+        else
+          puts "Fetching information about #{city_variable}"
+          categories =  JSON.parse(resp)["categories"]
+          categories.each do |c|
+            puts Rainbow("#{c['name']} : ").color(c['color']) + c['score_out_of_10'].to_i.to_s + " / 10"
+          end#each
+          #RETURN PERSON BACK TO MENU
+        end#rescue
+      elsif city_more=="n"#if
+        puts "Please use the website to apply for the job if you are interested. Search again if you wish to see other listings."
+    end#if
+  end#def
+
+
   # puts Rainbow("\nSymptoms of #{self.name}\n").color("#203259").bright + Rainbow(Format.wrap("\n\n#{self.symptoms[10..-1]}", 70)).color("#191921").gsub('\n  \t\t\n  \t\t', " ")
+
+
 
 
 ########################
