2de8c8ea569a8eecc2ee274bba43d1e121169d83
wimbleclaw <aisha305@hotmail.co.uk>
2018-10-03 14:01:58 +0100

    added pokeapi which populates pokemon table

--- bin/run.rb
+++ bin/run.rb
@@ -1,10 +1,10 @@
 require_relative '../config/environment'
 require_relative '../db/migrate/seed'
 
-
-get_character_properties_from_api("pikachu")
-get_user_pokemon("pikachu")
+# get_character_properties_from_api("pikachu")
+# get_user_pokemon("pikachu")
 # get_pokemon_name
 # get_pokemon_health
+create_pokemon_instances(pokemon_array)
 
 puts "EOF"
