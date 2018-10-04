b33aacdfdc04f8fd4a3ed9c60e5c8c3cbe54ff2f
wimbleclaw <aisha305@hotmail.co.uk>
2018-10-04 15:17:56 +0100

    changes to user input

--- bin/run.rb
+++ bin/run.rb
@@ -1,8 +1,9 @@
 require_relative '../config/environment'
 
 get_character_from_user(greeting, comp)
-find_users_pokemon
-find_comp_pokemon
+find_user_and_comp_pokemon
+
+winner_or_loser
 #user_table
 # delete_user_table
 # delete_userpokemon_table
